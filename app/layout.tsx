5e1000156534dba8ca1a299cc09b52894e97f094
Akeko. <artapp.theo@gmail.com>
2024-05-27 22:28:59 +0200

    refactor(seo): add opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Ubuntu } from "next/font/google";
+import { Roboto, Ubuntu } from "next/font/google";
 import Header from "./components/header/Header";
 import "./globals.css";
+import { baseUrl } from "./robots";
 
-const ubuntu = Ubuntu({
+export const ubuntu = Ubuntu({
   weight: ["400", "700"],
   style: ["normal", "italic"],
   subsets: ["latin"],
@@ -11,10 +12,37 @@ const ubuntu = Ubuntu({
   variable: "--font-ubuntu",
 });
 
+export const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+
 export const metadata: Metadata = {
   title: "Hello World - The developer's Swiss Army knife",
   description:
-    "Hello World is a site that gathers a set of tools to make developers' tasks easier!",
+    "Hello World is a site that gathers a set of tools to make developers tasks easier!",
+  icons: {
+    icon: "/icon.png",
+    apple: "/apple-icon.png",
+  },
+  manifest: `${baseUrl}/manifest.json`,
+  openGraph: {
+    title: "Hello World",
+    description:
+      "Hello World is a site that gathers a set of tools to make developers tasks easier!",
+    url: baseUrl,
+    siteName: "Hello World",
+    images: [
+      {
+        url: `${baseUrl}/hello-world.png`,
+      },
+    ],
+    type: "website",
+    locale: "fr_FR",
+  },
 };
 
 export default function RootLayout({
@@ -25,10 +53,12 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body
-        className={`${ubuntu.className} bg-waikawa-gray-50 h-dvh min-h-dvh text-waikawa-gray-950`}
+        className={`${ubuntu.className} bg-waikawa-gray-50 h-dvh min-h-dvh text-waikawa-gray-950 overflow-x-hidden`}
       >
         <Header />
-        <main className="py-8 px-8 lg:px-24">{children}</main>
+        <main className="flex flex-col gap-6 py-8 px-8 lg:px-24 max-w-6xl m-auto">
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+import CopyPasteInput from "./components/metadata/CopyPasteInput";
+import MetadataForm from "./components/metadata/MetadataForm";
+import PreviewFacebook from "./components/metadata/PreviewFacebook";
+import PreviewGoogle from "./components/metadata/PreviewGoogle";
+import PreviewTwitter from "./components/metadata/PreviewTwitter";
+
 const HomePage = async () => {
   return (
     <>
-      <h1 className="text-4xl font-bold text-waikawa-gray-950">
-        Hello World !
-      </h1>
+      <section className="flex flex-col gap-4">
+        <h1 className="text-5xl grid-rows-2 font-bold">Hello World !</h1>
+        <p>
+          Hello World is a site that gathers a set of tools to make developers
+          tasks easier!
+        </p>
+        <CopyPasteInput />
+      </section>
+      <section className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+        <div className="flex flex-col gap-4 min-w-96">
+          <h2 className="text-2xl font-bold">Metadata</h2>
+          <MetadataForm />
+        </div>
+        <div className="flex flex-col gap-4">
+          <h2 className="text-2xl font-bold">Preview</h2>
+          <PreviewGoogle />
+          <PreviewFacebook />
+          <PreviewTwitter />
+        </div>
+      </section>
     </>
   );
 };

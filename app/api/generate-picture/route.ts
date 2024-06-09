06f64b2ef56cc5afaae56714c299478a1bd683db
Akeko. <artapp.theo@gmail.com>
2024-06-09 13:03:00 +0200

    fix: upload dir

--- app/api/generate-picture/route.ts
+++ app/api/generate-picture/route.ts
@@ -1,7 +1,11 @@
-import fs from "node:fs/promises";
-import path from "node:path";
-
 // https://www.pronextjs.dev/next-js-file-uploads-server-side-solutions
+
+import { getFileNameFromUrl, stringifyAttributes } from "@/app/utils";
+import Image, { ImageFormat } from "@11ty/eleventy-img";
+import JSZip from "jszip";
+import fs from "node:fs/promises";
+import path from "path";
+
 export const POST = async (req: Request) => {
   try {
     let imagesToDelete = [];
@@ -10,109 +14,98 @@ export const POST = async (req: Request) => {
     const outputFormat = formData.get("format") as string;
     const arrayBuffer = await file.arrayBuffer();
     const buffer = new Uint8Array(arrayBuffer);
-    const uploadDir = path.join("/uploads");
+    const uploadDir = path.join(process.cwd(), "public", "uploads");
     const filePath = path.join(uploadDir, file.name);
 
-    // imagesToDelete.push(filePath);
+    imagesToDelete.push(filePath);
 
-    // await fs.mkdir(uploadDir, { recursive: true });
+    await fs.mkdir(uploadDir, { recursive: true });
     await fs.writeFile(filePath, buffer);
 
-    // const formats = ["webp", "jpeg"];
+    const formats = ["webp", "jpeg"];
 
-    // const imageMetadata = await Image(filePath, {
-    //   widths: [400, 800],
-    //   formats: [
-    //     "webp",
-    //     `${
-    //       outputFormat == "img/png"
-    //         ? "png"
-    //         : outputFormat == "img/jpeg"
-    //         ? "jpeg"
-    //         : "jpeg"
-    //     }`,
-    //   ],
-    //   outputDir: uploadDir,
-    //   urlPath: uploadDir,
-    // });
+    const imageMetadata = await Image(filePath, {
+      widths: [400, 800],
+      formats: [
+        "webp",
+        outputFormat === "img/png"
+          ? "png"
+          : outputFormat === "img/jpeg"
+          ? "jpeg"
+          : "jpeg",
+      ],
+      outputDir: uploadDir,
+      urlPath: "/uploads", // Chemin relatif pour accéder aux fichiers
+    });
 
-    // const getLargestImage = (format: ImageFormat) => {
-    //   const images = imageMetadata[format];
-    //   if (images) {
-    //     return images[images.length - 1];
-    //   }
-    // };
+    const getLargestImage = (format: ImageFormat) => {
+      const images = imageMetadata[format];
+      if (images) {
+        return images[images.length - 1];
+      }
+    };
 
-    // const sourceHtmlString = Object.values(imageMetadata)
-    //   .map((images) => {
-    //     const { sourceType } = images[0];
+    const sourceHtmlString = Object.values(imageMetadata)
+      .map((images) => {
+        const { sourceType } = images[0];
+        const sourceAttributes = stringifyAttributes({
+          type: sourceType,
+          srcset: images
+            .map((image) => `/uploads/${getFileNameFromUrl(image.srcset)}`)
+            .join(", "),
+          sizes: "100vw",
+        });
+        return `<source ${sourceAttributes}>`;
+      })
+      .join("\n");
 
-    //     const sourceAttributes = stringifyAttributes({
-    //       type: sourceType,
-    //       srcset: images
-    //         .map(
-    //           (image) => `/your-path-folder/${getFileNameFromUrl(image.srcset)}`
-    //         )
-    //         .join(", "),
-    //       sizes: "100vw",
-    //     });
+    const largestUnoptimizedImg = getLargestImage(formats[0] as ImageFormat);
+    const imgAttributes = stringifyAttributes({
+      src: `/uploads/${getFileNameFromUrl(largestUnoptimizedImg?.url)}`,
+      width: largestUnoptimizedImg?.width,
+      height: largestUnoptimizedImg?.height,
+      loading: "lazy",
+      decoding: "async",
+    });
+    const imgHtmlString = `<img ${imgAttributes}>`;
 
-    //     return `<source ${sourceAttributes}>`;
-    //   })
-    //   .join("\n");
+    const picture = `<picture>
+      ${sourceHtmlString}
+      ${imgHtmlString}
+    </picture>`;
 
-    // const largestUnoptimizedImg = getLargestImage(formats[0] as ImageFormat);
-    // const imgAttributes = stringifyAttributes({
-    //   src: `/your-path-folder/${getFileNameFromUrl(
-    //     largestUnoptimizedImg?.url
-    //   )}`,
-    //   width: largestUnoptimizedImg?.width,
-    //   height: largestUnoptimizedImg?.height,
-    //   loading: "lazy",
-    //   decoding: "async",
-    // });
-    // const imgHtmlString = `<img ${imgAttributes}>`;
+    const zip = new JSZip();
+    zip.file("picture.html", picture);
+    const imgDir = zip.folder("images");
 
-    // const picture = `<picture>
-    //   ${sourceHtmlString}
-    //   ${imgHtmlString}
-    // </picture>`;
+    await Promise.all(
+      Object.values(imageMetadata)
+        .flat()
+        .map(async (image) => {
+          const imagePath = path.join(
+            uploadDir,
+            getFileNameFromUrl(image.outputPath) as string
+          );
+          imagesToDelete.push(imagePath);
+          const imageBuffer = await fs.readFile(imagePath);
+          imgDir?.file(
+            getFileNameFromUrl(image.outputPath) as string,
+            imageBuffer
+          );
+        })
+    );
 
-    // const zip = new JSZip();
-    // zip.file("picture.html", picture);
-    // const imgDir = zip.folder("images");
-
-    // await Promise.all(
-    //   Object.values(imageMetadata)
-    //     .flat()
-    //     .map(async (image) => {
-    //       const imagePath = path.join(
-    //         uploadDir,
-    //         getFileNameFromUrl(image.outputPath) as string
-    //       );
-    //       imagesToDelete.push(imagePath);
-    //       const imageBuffer = await fs.readFile(imagePath);
-    //       imgDir?.file(
-    //         getFileNameFromUrl(image.outputPath) as string,
-    //         imageBuffer
-    //       );
-    //     })
-    // );
-
-    // const zipContent = await zip.generateAsync({ type: "nodebuffer" });
-    // imagesToDelete.forEach((image) => fs.unlink(image));
-    // return new Response(zipContent, {
-    //   status: 200,
-    //   headers: {
-    //     "Content-Type": "application/zip",
-    //     "Content-Disposition": `attachment; filename="snapcial-ai.zip"`,
-    //   },
-    // });
-    return new Response(JSON.stringify({ message: "Bordel" }), {
+    const zipContent = await zip.generateAsync({ type: "nodebuffer" });
+    imagesToDelete.forEach((image) => fs.unlink(image));
+    return new Response(zipContent, {
       status: 200,
+      headers: {
+        "Content-Type": "application/zip",
+        "Content-Disposition": `attachment; filename="snapcial-ai.zip"`,
+      },
     });
   } catch (e: any) {
-    console.error(e);
+    console.error("Error during POST request:", e);
     return new Response(JSON.stringify({ message: e.message }), {
       status: 500,
       headers: { "Content-Type": "application/json" },

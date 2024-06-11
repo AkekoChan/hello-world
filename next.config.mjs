f627bfe5c1f9a80c677b80198a1c8091ff395900
Akeko. <artapp.theo@gmail.com>
2024-06-11 19:42:14 +0200

    fix: upload dir

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    nftTracing: true,
+    outputFileTracingIncludes: {
+      "/api/generate-picture": ["./public/uploads/**/*"],
+    },
   },
 };
 

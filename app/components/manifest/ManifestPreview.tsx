66b25581ad61a9a82ca19e300d7a68b8c69a44c1
Akeko. <artapp.theo@gmail.com>
2025-03-12 12:08:05 +0100

    refactor(manifest): update HTML output

--- app/components/manifest/ManifestPreview.tsx
+++ app/components/manifest/ManifestPreview.tsx
@@ -56,9 +56,31 @@ const ManifestPreview = () => {
         <pre className="ps-4">
           <code className="text-waikawa-gray-800">
             <span className="font-bold">&quot;description&quot;</span>: &quot;
-            {data?.description}&quot;
+            {data?.description}&quot;,
           </code>
         </pre>
+        <p className="ps-4">
+          <code className="text-waikawa-gray-800">
+            <span className="font-bold">&quot;icons&quot;</span>: &#91; <br />{" "}
+            &#123; <br />
+            <span className="font-bold ps-4">&quot;src&quot;</span>:
+            &quot;your-image-folder/favicons/favicon-512.png&quot;, <br />
+            <span className="font-bold ps-4">&quot;sizes&quot;</span>:
+            &quot;512x512&quot;, <br />
+            <span className="font-bold ps-4">&quot;type&quot;</span>:
+            &quot;image/png&quot; <br />
+            <span className="ps-4">&#125;</span>, <br />
+            <span className="ps-4">&#123;</span> <br />
+            <span className="font-bold ps-4">&quot;src&quot;</span>:
+            &quot;your-image-folder/favicons/favicon-192.png&quot;, <br />
+            <span className="font-bold ps-4">&quot;sizes&quot;</span>:
+            &quot;192x192&quot;, <br />
+            <span className="font-bold ps-4">&quot;type&quot;</span>:
+            &quot;image/png&quot; <br />
+            <span className="ps-4">&#125;</span>
+            <br /> &#93;
+          </code>
+        </p>
         <pre>
           <code className="text-waikawa-gray-800">&#125;</code>
         </pre>
